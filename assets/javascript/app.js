ff4502ecc75858146953696522cd034420acbde5
Dani Carter <carte470@d.umn.edu>
2017-12-20 21:01:05 -0600

    images added and setTimout fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,56 +1,61 @@
 var correct = 0;
-
 var wrong = 0;
-
 var unanswered = 0;
 
 var myQuestions = [
 
 	{ "question": "What year was Phi Sigma Sigma founded?",
 		"answers": ['1912','1913','1920','1921'],
-		"correctAnswer":'1913'
+		"correctAnswer":'1913',
+		"image": 'assets/images/1913.jpg'
 	},
-
 	{ "question": "At what school was Phi Sigma Sigma founded?",
 		"answers": ['Hunter College','New York University','Cornell University','Columbia University'],
-		"correctAnswer": 'Hunter College'
+		"correctAnswer": 'Hunter College',
+		"image": 'assets/images/huntercollege.jpg'
 	},
+	{ "question": "What is Phi Sigma Sigma's official stone?",
+		"answers": ['Ruby','Diamond','Emerald','Sapphire'],
+		"correctAnswer": 'Sapphire',
+		"image": 'assets/images/sapphire.jpg'
 
-	{ "question": "Who is not a founder of Phi Sigma Sigma?",
-		"answers": ['Fay Chertkoff','Jeanette Lipka Furst','Estelle Melnick Cole','Shirley Ann Frederick'],
-		"correctAnswer": 'Shirley Ann Frederick'
 	},
-
 	{ "question": "How many founders does Phi Sigma Sigma have?",
 		"answers": ['12','3','10','6'],
-		"correctAnswer": '10'
+		"correctAnswer": '10',
+		"image": 'assets/images/founders.jpg'
 	},
-
 	{ "question": "What are Phi Sigma Sigma's colors?",
 		"answers": ['Navy and Gold','King Blue and Gold','Rose Red and Sunshine Yellow','Black and White'],
-		"correctAnswer": 'King Blue and Gold'
+		"correctAnswer": 'King Blue and Gold',
+		"image": 'assets/images/badge.jpg'
 	},
-
 	{ "question": "Where is Phi Sigma Sigma Headquarters located?",
 		"answers": ['Baltimore, MD','Washington, D.C.','Elkridge, MD','New York City, NY'],
-		"correctAnswer": 'Elkridge, MD'
+		"correctAnswer": 'Elkridge, MD',
+		"image": 'assets/images/headquarters.jpg'
 	},
-
 	{ "question": "What is Phi Sigma Sigma's motto?",
-	"answers": ['Diokete Upsala','Union hand in hand','Do Good','Seek the Noblest'],
-	"correctAnswer": 'Diokete Upsala'
+		"answers": ['Diokete Hupsala','Union hand in hand','Do Good','Seek the Noblest'],
+		"correctAnswer": 'Diokete Hupsala',
+		"image": 'assets/images/crest.JPG'
+	},
+	{ "question": "What does Diokete Hupsala mean?",
+		"answers": ['Once, Always','Aim High','Do Good','Seek the Noblest'],
+		"correctAnswer": 'Diokete Hupsala',
+		"image": 'assets/images/aimhigh.jpg'
 	},
 
 	{ "question": "What is Phi Sigma Sigma's symbol?",
 		"answers": ['Skull & Crossbones','Anchor','Arrow','Sphinx'],
-		"correctAnswer": 'Sphinx'
+		"correctAnswer": 'Sphinx',
+		"image": 'assets/images/sphinx.jpg'
 	},
-
 	{ "question": "What does LITP mean?",
 		"answers": ['Lean into thy prayer','Love in the Pyramid','Learning in the purest ','Lean into the Phi'],
 		"correctAnswer": 'Love in the Pyramid'
+		"image": 'assets/images/logo.jpeg'
 	},
-
 ];
 
 $(document).ready(function(){
@@ -63,11 +68,9 @@ $(document).ready(function(){
 	startBtn.appendChild(text);
 
 	$("#btn").append(startBtn);
-
 });
 
-var number = 16;
-
+var number = 4;
 var intervalId;
 
 function start(){
@@ -77,55 +80,59 @@ function start(){
 		$("#btn").hide();
 
 		displayQuestion(0);
-
 	})
 };
 
 function setTimer() {
 
-	number = 16;
-
+	number = 4;
 	intervalId = setInterval(decrement, 1000);
 
 		console.log("Timer set.");
-
 };
 
 //function showQuestions- onclick of button
 function displayQuestion(qIndex) {
 
-		setTimer();
+	setTimer();
 
-		$("#question").html(myQuestions[qIndex].question);
+	$("#timer").show();
 
-		//reset html of answer div to blank before adding answer-buttons of next question
-		$("#answer").html('');
+	//reset results div to blank before displaying next question
+	$("#results").empty();
 
-		for (var i = 0; i < myQuestions[qIndex].answers.length; i++) {
+	$("#pic").hide();
 
-			 var answerBtn = $("<button>");
+	$("#question").html(myQuestions[qIndex].question);
 
-			 answerBtn.addClass("answer-button");
+	//reset html of answer div to blank before adding answer-buttons of next question
+	$("#answer").empty();
 
-			 answerBtn.attr("data-answer", myQuestions[qIndex].answers[i]);
+	for (var i = 0; i < myQuestions[qIndex].answers.length; i++) {
 
-			 answerBtn.text(myQuestions[qIndex].answers[i]);
+		 var answerBtn = $("<button>");
 
-			 answerBtn.click(checkAnswer);
+		 answerBtn.addClass("answer-button");
 
-			 $("#answer").append(answerBtn);
-		};
+		 answerBtn.attr("data-answer", myQuestions[qIndex].answers[i]);
+
+		 answerBtn.text(myQuestions[qIndex].answers[i]);
+
+		 answerBtn.click(checkAnswer);
+
+		 $("#answer").append(answerBtn);
+	};
 };
 
 var qIndex= 0;
+
 
 function checkAnswer(){
 
 	console.log("clicked2");
 
 	var userSelect = $(this).attr("data-answer");
-
-	var correctAnswer = myQuestions[qIndex].correctAnswer; 
+	var correctAnswer = myQuestions[qIndex].correctAnswer;
 
 	if(userSelect === correctAnswer) {
 
@@ -137,67 +144,79 @@ function checkAnswer(){
 };
 	
 function showResult(result) {
-	if(result === "correct") {
 
-		console.log("Correct!");
+	if(result === "correct") {
 
 		correct++;
 
-		console.log("Number correct: " + correct);
+		//disable buttons clickability
+		$(".answer-button").hide();
 
 		nextQuestion();
+
+		$("#results").html("Correct!");
+
+		$("#pic").show();
+
+		$("#pic").attr('src', myQuestions[qIndex].image);
 	}
 	else if(result === "incorrect") {
 
-		console.log("Incorrect!");
-
 		wrong++;
 
-		console.log("Number wrong: " + wrong);
+		//hide buttons so they cannot be clicked again
+		$(".answer-button").hide();
 
 		nextQuestion();
+
+		$("#results").html("Nope! The correct answer was " + myQuestions[qIndex].correctAnswer + ".");
 	}
 	else {
-		console.log("No answer selected!");
 
 		unanswered++;
 
-		console.log("Number unanswered: " + unanswered);
+		//hide buttons so they cannot be clicked again
+		$(".answer-button").hide();
 
 		nextQuestion();
-	}
+
+		$("#results").html("No answer selected! The correct answer was " + myQuestions[qIndex].correctAnswer + ".");
+	};
 };
 
 function nextQuestion() {
 
-	qIndex++;
+	clearInterval(intervalId);
 
-	if (qIndex <= (myQuestions.length -1)) {
+	$("#timer").hide();
 
-		stop();
+	setTimeout(function() {
 
-		//move to next question automatically 
-		displayQuestion(qIndex);
+		qIndex++;
 
+		if (qIndex <= (myQuestions.length -1)) {
 
-		console.log("nextQ");
-	}
+			stop();
 
-	else {
+			//move to next question automatically 
+			displayQuestion(qIndex);
 
-		clearInterval(intervalId);
+			console.log("nextQ");
+		}
+		else {
 
-		console.log("end of round");
+			clearInterval(intervalId);
 
-		// //Show results
-		$("#results").html("<h2>Finished! Here's how you did: <br> Correct:" + correct + "<br> Incorrect: " + wrong + "<br> Unanswered: " + unanswered + "</h2>");
+			console.log("end of round");
 
-		//disable buttons clickability
-		$(".answer-button").off('click');
+			// //Show results
+			$("#results").html("<h2>Finished! Here's how you did: <br> Correct:" + correct + "<br> Incorrect: " + wrong + "<br> Unanswered: " + unanswered + "</h2>");
 
-		$("#timer").hide();
+			$("#question").hide();
 
-	};	
+			$("#timer").hide();
+		};
+	}, 5000);	
 };
 
 //Set timer to decrease by 1 second until 0. 
@@ -209,28 +228,17 @@ function decrement() {
 
 	if (number === 0) {
 		showResult();
-		// nextQuestion("0");
-
 		console.log("Time Up!");
-
-
-
 	} 
-
 };
-
-//function showResults- onclick of answer- use if/else, set time result shows on screen ~5seconds
-// function showResults() {
-	
-// };
 
 function stop(){
 
 	console.log("Timer stopped.")
 
 	clearInterval(intervalId);
-
 };
 
 start();
+
 
